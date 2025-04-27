6860ce7d5e315c8265a5e471b69bab4e489dae45
tjhootman <tjhootman@gmail.com>
2025-04-27 19:55:42 -0400

    Plot Global Fires using plotly

--- fire_map.py
+++ fire_map.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import csv
 from datetime import datetime
+import plotly.express as px
 
 path = Path('fire_data/world_fires_1_day.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
@@ -15,11 +16,16 @@ lats = []
 dates = []
 
 for row in reader:
-    lon = float(row[0])
-    lat = float(row[1])
+    lat = float(row[0])
+    lon = float(row[1])
     bright = float(row[2])
     date = datetime.strptime(row[5], '%Y-%m-%d')
     lons.append(lon)
     lats.append(lat)
     brights.append(bright)
     dates.append(date)
+
+title = 'Global Fires'
+fig = px.scatter_geo(lat=lats, lon=lons, title=title)
+
+fig.show()
