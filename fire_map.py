275c29f1c1e940a541a27a982186e77fceb7cdfb
tjhootman <tjhootman@gmail.com>
2025-04-27 20:50:58 -0400

    Add custom_data and hovertemplate. Limit to first 1000 lines from CSV

--- fire_map.py
+++ fire_map.py
@@ -9,24 +9,33 @@ lines = path.read_text(encoding='utf-8').splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# extract brights
-brights = []
-lons = []
+# extract data
 lats = []
+lons = []
+brights = []
 dates = []
 hover_texts = []
 
+# counter for rows
+row_count = 0
+max_rows = 1000
+
 for row in reader:
+    if row_count >= max_rows:
+        break
+
     lat = float(row[0])
     lon = float(row[1])
     bright = float(row[2])
-    date = datetime.strptime(row[5], '%Y-%m-%d')
+    date = datetime.strptime(row[5], '%Y-%m-%d').date()
     hover_text = f'{lat}, {lon}/n{date}'
     lons.append(lon)
     lats.append(lat)
     brights.append(bright)
     dates.append(date)
     hover_texts.append(hover_text)
+
+    row_count += 1
 
 title = 'Global Fires'
 fig = px.scatter_geo(lat=lats,
@@ -36,6 +45,13 @@ fig = px.scatter_geo(lat=lats,
                      color_continuous_scale='Bluered_r',
                      labels={'color':'Brightness'},
                      projection='natural earth',
+                     custom_data=[lats, lons, dates]
                      )
+fig.update_traces(
+                hovertemplate=
+                '(%{customdata[0]}°, ' +
+                '%{customdata[1]}°)<br>' +
+                '%{customdata[2]}'
+                )
 
 fig.show()
