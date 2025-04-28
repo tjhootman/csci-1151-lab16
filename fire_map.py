fea285ccb6c8298e5af4a7acbdd860c5f1803285
tjhootman <tjhootman@gmail.com>
2025-04-27 22:07:19 -0400

    Add documentation

--- fire_map.py
+++ fire_map.py
@@ -3,41 +3,57 @@ import csv
 from datetime import datetime
 import plotly.express as px
 
+# define the path to csv file
 path = Path('fire_data/world_fires_1_day.csv')
+
+# read the contents of file and split into lines
 lines = path.read_text(encoding='utf-8').splitlines()
 
+# create csv reader to iterate over the lines. skip header row
 reader = csv.reader(lines)
-header_row = next(reader)
+next(reader)
 
-# extract data
+# initalize empty lists to store extracted data
 lats = []
 lons = []
 brights = []
 dates = []
 hover_texts = []
 
-# counter for rows
+# counter for rows and limit for data processing
 row_count = 0
 max_rows = 1000
 
+# iterate over each row in CSV reader
 for row in reader:
+    # stop processing if maximum rows is reached
     if row_count >= max_rows:
         break
 
-    lat = float(row[0])
-    lon = float(row[1])
-    bright = float(row[2])
-    date = datetime.strptime(row[5], '%Y-%m-%d').date()
-    hover_text = f'{lat}, {lon}/n{date}'
-    lons.append(lon)
-    lats.append(lat)
-    brights.append(bright)
-    dates.append(date)
-    hover_texts.append(hover_text)
+    # extract data from specific columns and convert to appropriate types
+    try:
+        lat = float(row[0])
+        lon = float(row[1])
+        bright = float(row[2])
+        date = datetime.strptime(row[5], '%Y-%m-%d').date()
+        hover_text = f'{lat}, {lon}/n{date}' # is this needed?
+    except ValueError as e:
+        print(e)
+    else:
+        # append the data the appropriate lists
+        lons.append(lon)
+        lats.append(lat)
+        brights.append(bright)
+        dates.append(date)
+        hover_texts.append(hover_text)
 
+    # increment the row counter
     row_count += 1
 
+# set the title
 title = 'Global Fires'
+
+# create a geographical scatter plot
 fig = px.scatter_geo(lat=lats,
                      lon=lons,
                      title=title,
@@ -47,11 +63,18 @@ fig = px.scatter_geo(lat=lats,
                      projection='natural earth',
                      custom_data=[lats, lons, dates]
                      )
+
+# update the markers and hover text information
 fig.update_traces(
+                marker=dict(size=10,line=dict(width=1,color='White')),
+                selector=dict(mode='markers'),
                 hovertemplate=
                 '(%{customdata[0]}°, ' +
                 '%{customdata[1]}°)<br>' +
-                '%{customdata[2]}'
+                '%{customdata[2]}',
                 )
 
+# show the plot
 fig.show()
+
+# additional try-except blocks?
