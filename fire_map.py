62fa909d357ce8ad52a9d78ceb6bd5373543bf1a
tjhootman <tjhootman@gmail.com>
2025-04-27 20:14:51 -0400

    Add color scale and projection to map

--- fire_map.py
+++ fire_map.py
@@ -14,18 +14,28 @@ brights = []
 lons = []
 lats = []
 dates = []
+hover_texts = []
 
 for row in reader:
     lat = float(row[0])
     lon = float(row[1])
     bright = float(row[2])
     date = datetime.strptime(row[5], '%Y-%m-%d')
+    hover_text = f'{lat}, {lon}/n{date}'
     lons.append(lon)
     lats.append(lat)
     brights.append(bright)
     dates.append(date)
+    hover_texts.append(hover_text)
 
 title = 'Global Fires'
-fig = px.scatter_geo(lat=lats, lon=lons, title=title)
+fig = px.scatter_geo(lat=lats,
+                     lon=lons,
+                     title=title,
+                     color=brights,
+                     color_continuous_scale='Bluered_r',
+                     labels={'color':'Brightness'},
+                     projection='natural earth',
+                     )
 
 fig.show()
